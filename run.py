bf50c5256213da9577b3ba95cb27be49360b5486
Michał Kuźba <kmichael00968@gmail.com>
2017-03-16 23:43:01 +0100

    Working for powiaty and gminy

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from xlrd import open_workbook
 from collections import OrderedDict
 from json import dumps
 
+CANDIDATES_NUM = 12
 
 templateLoader = FileSystemLoader( searchpath="templates" )
 
@@ -19,6 +20,8 @@ sheet = open_workbook('dane/gm-kraj.xls').sheet_by_index(0)
 
 candidates = [sheet.cell(0, i).value for i in range(10, 22)]
 
+rubryki = ['Uprawnieni do głosowania', 'Wydane karty', 'Wyjęto z urny', 'Nieważne głosy', 'Ważne głosy', 'Frekwencja']
+
 def generuj_gminy():
     for row in range(1, sheet.nrows):
         FILE = 'gmina' + sheet.cell(row, 1).value + '.html'
@@ -30,7 +33,6 @@ def generuj_gminy():
         gmina = sheet.cell(row, 2).value
         powiat = sheet.cell(row, 3).value
 
-        rubryki = ['Uprawnieni do głosowania', 'Wydane karty', 'Wyjęto z urny', 'Nieważne głosy', 'Ważne głosy', 'Frekwencja']
         wartosci = []
 
         for col in range(5, 10):
@@ -40,23 +42,72 @@ def generuj_gminy():
 
         ogolne = OrderedDict(zip(rubryki, wartosci))
 
-        lista = [['Kandydat', 'Głosy']]
+        diagram = [['Kandydat', 'Głosy']]
 
         for kandydat, glosy in zip(candidates, votes):
-            lista.append([kandydat, glosy])
+            diagram.append([kandydat, glosy])
 
 
-        outputText = template.render({'res_dict' : res_dict, 'gmina' : gmina, 'powiat' : powiat, 'ogolne' : ogolne, 'lista' : dumps(lista)})
+        outputText = template.render({'res_dict' : res_dict, 'gmina' : gmina, 'powiat' : powiat, 'ogolne' : ogolne, 'diagram' : dumps(diagram)})
 
 
         with open('pages/gminy/' + FILE, 'w') as page:
             page.write(outputText)
 
+class Powiat:
+
+    def __init__(self, kod, nazwa):
+        self.kod = kod
+        self.nazwa = nazwa
+        self.gminy = []
+        self.votes = [0] * CANDIDATES_NUM
+        self.ogolne = OrderedDict(zip(rubryki, [0] * 6))
+        self.res_dict = OrderedDict(zip(candidates, self.votes))
+
+    """ Update given a row. """
+    def update(self, row):
+        self.gminy.append(row[2].value)
+        for g1, i in zip(row[10:22], range(CANDIDATES_NUM)):
+            self.votes[i] += int(g1.value)
+
+        for key, g1 in zip(self.res_dict.keys(), self.votes):
+            self.res_dict[key] = g1
+
+        wartosci = []
+
+        for col in range(5, 10):
+            wartosci.append(int(row[col].value))
+
+        for rubryka, wartosc in zip(rubryki[:-1], wartosci):
+            self.ogolne[rubryka] += wartosc
+
+        self.ogolne['Frekwencja'] = "%.2f" % (self.ogolne['Wydane karty'] / self.ogolne['Uprawnieni do głosowania'] * 100)
 
 def generuj_powiaty():
-    pass
+    powiaty = OrderedDict()
+    for row in range(1, sheet.nrows):
+        kod = sheet.cell(row, 1).value[:4]
+        nazwa = sheet.cell(row, 2).value
+        powiaty[kod] = Powiat(kod, nazwa)
+
+    for row in range(1, sheet.nrows):
+        kod = sheet.cell(row, 1).value[:4]
+        powiaty[kod].update(sheet.row(row))
 
 
+    for powiat in powiaty.values():
+        FILE = 'powiat' + powiat.kod + '.html'
+
+
+        diagram = [['Kandydat', 'Głosy']]
+
+        for kandydat, glosy in zip(candidates, powiat.votes):
+            diagram.append([kandydat, glosy])
+
+        outputText = template.render({'res_dict' : powiat.res_dict, 'powiat' : powiat.nazwa, 'ogolne' : powiat.ogolne, 'diagram' : dumps(diagram)})
+
+        with open('pages/powiaty/' + FILE, 'w') as page:
+            page.write(outputText)
 
 
 
